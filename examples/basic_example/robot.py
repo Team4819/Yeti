c288c11cf9c3e567c51ddec644e9b1da501d4b97
Christian J. Balcom <robot.inventor@gmail.com>
2014-12-19 09:18:32 -0500

    Trimmed fat from the example robot.py, added docs to hook_server

--- examples/basic_example/robot.py
+++ examples/basic_example/robot.py
@@ -1,9 +1,8 @@
 import wpilib
 import yeti
-from yeti.module_interfaces import events
 
 
-class George(wpilib.IterativeRobot):
+class LittleRobot(wpilib.IterativeRobot):
 
     def robotInit(self):
         context = yeti.Context()
@@ -11,13 +10,7 @@ class George(wpilib.IterativeRobot):
         config_manager = yeti.ConfigManager()
         config_manager.parse_config_file("mods.conf")
         config_manager.load_startup_mods(context)
-        wpilib.Timer.delay(3)
-        events.trigger_event_threadsafe("tick", context)
-        #module_loader = yeti.ModuleLoader()
-        #module_loader.set_context(context)
-        #module_loader.load("examples.example_module_fail")
-
 
 
 if __name__ == "__main__":
-    wpilib.run(George)
+    wpilib.run(LittleRobot)
--- yeti/hook_server.py
+++ yeti/hook_server.py
@@ -1,25 +1,81 @@
 import logging
 
 
+class Hook(object):
+    """
+    This provides an interface for a method used as a hook with a :class:HookServer
+    """
+
+    def __init__(self, hook_server, hook_name, func):
+        """
+        :param hook_server: The instance of :class:HookServer that uses this hook.
+        :param hook_id: The
+        :param func: The function to use with the hook.
+        """
+        self.func = func
+        self.hook_server = hook_server
+        self.hook_name = hook_name
+
+    def call(self, *args, **kwargs):
+        """
+        Triggers all hooks registered with hook_server for hook_name
+        """
+        self.hook_server.call_hook(*args, **kwargs)
+
+    def unset(self):
+        """
+        Removes the trigger from hook_server
+        """
+        self.hook_server.remove_hook(self)
+
+
 class HookServer(object):
+    """
+    This provides a convenient hook registration mechanism.
+    """
 
     def __init__(self):
         self.logger = logging.getLogger("yeti." + self.__class__.__name__)
-        self.hooks = dict()
+        self._hooks = dict()
 
     def add_hook(self, hook_name, callback):
-        if hook_name not in self.hooks:
-            self.hooks[hook_name] = list()
-        self.hooks[hook_name].append(callback)
+        """
+        :param hook_name: The name of the function to create a hook for.
+        :param callback: The method to use for the hook.
+        Adds callback to the list of hooks to be run when hook_name is called.
+        :returns The created instance of :class:Hook
+        """
+        hook = Hook(self, hook_name, callback)
+        if hook_name not in self._hooks:
+            self._hooks[hook_name] = list()
+        self._hooks[hook_name].append(hook)
+        return hook
 
     def call_hook(self, hook_name, *args, **kwargs):
+        """
+        :param hook_name: The name of hook to be called
+        Calls all hooks that have been added to this :class:HookServer under the name hook_name with all extra
+        provided parameters.
+        :returns If at least one hook was successfully called.
+        """
         retval = False
-        if hook_name in self.hooks:
-            for hook in self.hooks[hook_name]:
+        if hook_name in self._hooks:
+            for hook in self._hooks[hook_name]:
                 try:
                     hook(*args, **kwargs)
                     retval = True
                 except Exception as e:
                     self.logger.exception("Exception on hook call: {}".format(e))
         return retval
+
+    def remove_hook(self, hook):
+        """
+        :param hook: The :class:Hook object to remove
+        :returns Weather or not removal was successful.
+        Removes hook from the instance of :class:HookServer
+        """
+        if hook in self._hooks[hook.hook_name]:
+            self._hooks[hook.hook_name].remove(hook)
+            return True
+        return False
 
